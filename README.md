a7561ef30a05219ea346456a3e9a4c77c4b452e3
debasmit <debasmit21@gmail.com>
2023-09-20 20:33:29 +0530

    third commit

--- README.md
+++
@@ -1,2 +0,0 @@
-This is the initial version of the Nasha mukti Database system ; s frontend 
-Database isnt ready yet 
--- src/components/Navbars/DemoNavbar.js
+++ src/components/Navbars/DemoNavbar.js
@@ -75,7 +75,7 @@ class DemoNavbar extends React.Component {
               <NavbarBrand className="mr-lg-5" to="/" tag={Link}>
                 <img
                   alt="..."
-                  src={require("assets/img/brand/775954_newliferehabanddeaddictioncentre1566246932-removebg-preview (1).png")}
+                  src={require("assets/img/brand/775954.png")}
                 />
               </NavbarBrand>
               <button className="navbar-toggler" id="navbar_global">
@@ -94,7 +94,7 @@ class DemoNavbar extends React.Component {
                       <Link to="/">
                         <img
                           alt="..."
-                          src={require("assets/img/brand/775954_newliferehabanddeaddictioncentre1566246932-removebg-preview (1).png")}
+                          src={require("assets/img/brand/775954.png")}
                         />
                       </Link>
                     </Col>
